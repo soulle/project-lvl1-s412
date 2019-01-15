a6cbdfeabc1f5fe9a92b468d4e6193d0f8f7a962
Saule <saulekazanbaeva3110@gmail.com>
2019-01-15 23:23:10 +0300

    add bad brain-even

--- src/index.js
+++ src/index.js
@@ -5,4 +5,39 @@ const hello = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
+
+const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+const game = (counter, name) => {
+  const num = getRandomNum(1, 50);
+  console.log(`Question: ${num}`);
+  const correctAnswer = (num) => num % 2 === 0 ? 'yes' : 'no';
+
+  const answer = readlineSync.question('Your answer: ');
+  
+  if (answer === correctAnswer) {
+    console.log('Correct!');
+    
+    if (counter === 3) {
+      console.log(`Congratulations, ${name}`);
+    }
+  } else {
+    console.log(`${answer} was wrong answer ;(. Correct answer was ${correctAnswer}`);
+  }
+  return game(counter + 1);
+};
+
+const evenGame = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no"');
+
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+
+  return game(0, name);
+};
+
+export { evenGame };
 export default hello;
+
+
