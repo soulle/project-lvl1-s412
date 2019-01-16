2f0286c8f784ec9dc78821bbf3a18f9587ea034f
soulle <saulekazanbaeva3110@gmail.com>
2019-01-16 18:59:57 +0300

    fix remarks for step4

--- src/index.js
+++ src/index.js
@@ -8,30 +8,29 @@ const hello = () => {
 
 const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-const getCorrect = number => ((number % 2 === 0) ? 'yes' : 'no');
+const isEven = number => number % 2 === 0;
+
+const getCorrect = number => (isEven(number) ? 'yes' : 'no');
 
 const evenGame = () => {
   console.log('Welcome to the Brain Games!');
   console.log('Answer "yes" if number even otherwise answer "no"');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  let round = 1;
-  while (round <= 3) {
-    const num = getRandomNum(1, 50);
-    console.log(`Question: ${num}`);
-    const correctAnswer = getCorrect(num);
+  const maxNumberOfRound = 3;
+  for (let round = 1; round <= maxNumberOfRound; round += 1) {
+    const question = getRandomNum(1, 50);
+    console.log(`Question: ${question}`);
+    const correctAnswer = getCorrect(question);
     const answer = readlineSync.question('Your answer: ');
     if (answer === correctAnswer) {
       console.log('Correct!');
-      if (round === 3) {
-        console.log(`Congratulations, ${name}!`);
-      }
-      round += 1;
     } else {
       console.log(`'${answer}' was wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
-      break;
+      return;
     }
   }
+  console.log(`Congratulations, ${name}!`);
 };
 
 export { evenGame };
