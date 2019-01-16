6426be645b037940b79712aa6d515034b1971874
soulle <saulekazanbaeva3110@gmail.com>
2019-01-16 14:13:47 +0300

    add working brain-even

--- src/index.js
+++ src/index.js
@@ -8,37 +8,30 @@ const hello = () => {
 
 const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-const getCorrectAnswer = (number) => {
-  if (number % 2 === true) {
-    return 'yes';
-  }
-  return 'no';
-};
-
-const game = (counter, name) => {
-  const num = getRandomNum(1, 50);
-  console.log(`Question: ${num}`);
-  const correctAnswer = getCorrectAnswer(num);
-  const answer = readlineSync.question('Your answer: ');
-  if (answer === correctAnswer) {
-    console.log('Correct!');
-    if (counter === 3) {
-      console.log(`Congratulations, ${name}`);
-    }
-  } else {
-    console.log(`${answer} was wrong answer ;(. Correct answer was ${correctAnswer}`);
-  }
-  return game(counter + 1);
-};
+const getCorrect = number => ((number % 2 === 0) ? 'yes' : 'no');
 
 const evenGame = () => {
   console.log('Welcome to the Brain Games!');
   console.log('Answer "yes" if number even otherwise answer "no"');
-
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-
-  return game(0, name);
+  let round = 1;
+  while (round <= 3) {
+    const num = getRandomNum(1, 50);
+    console.log(`Question: ${num}`);
+    const correctAnswer = getCorrect(num);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+      if (round === 3) {
+        console.log(`Congratulations, ${name}!`);
+      }
+      round += 1;
+    } else {
+      console.log(`${answer} was wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${name}!`);
+      break;
+    }
+  }
 };
 
 export { evenGame };
