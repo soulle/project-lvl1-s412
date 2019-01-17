a83a419ee6b92f1b14159e48502cd0f86307140b
soulle <saulekazanbaeva3110@gmail.com>
2019-01-17 09:41:27 +0300

    add brain-calc

--- src/bin/brain-even.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-import { evenGame } from '..';
-
-evenGame();
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
+import { cons, car, cdr } from 'hexlet-pairs';
 
 const hello = () => {
   console.log('Welcome to the Brain Games!');
@@ -6,32 +7,95 @@ const hello = () => {
   console.log(`Hello, ${name}!`);
 };
 
+const description = (nameOfTheGame) => {
+  switch (nameOfTheGame) {
+    case 'brain-even':
+      console.log('Answer "yes" if number even otherwise answer "no"');
+      break;
+    case 'brain-calc':
+      console.log('What is the result of the expression?');
+      break;
+    // no default
+  }
+};
+
+const start = (nameOfTheGame) => {
+  console.log('Welcome to the Brain Games!');
+  description(nameOfTheGame);
+};
+
+const greeting = name => `Hello, ${name}`;
+
 const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
 const isEven = number => number % 2 === 0;
 
 const getCorrect = number => (isEven(number) ? 'yes' : 'no');
 
-const evenGame = () => {
-  console.log('Welcome to the Brain Games!');
-  console.log('Answer "yes" if number even otherwise answer "no"');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  const maxNumberOfRound = 3;
-  for (let round = 1; round <= maxNumberOfRound; round += 1) {
+const maxNumberOfRound = 3;
+
+const toString = answer => (((typeof answer) === 'string') ? answer : String(answer));
+
+const makeQuestionAndCorrectAnswer = (nameOfTheGame) => {
+  if (nameOfTheGame === 'brain-even') {
     const question = getRandomNum(1, 50);
-    console.log(`Question: ${question}`);
     const correctAnswer = getCorrect(question);
+    return cons(question, correctAnswer);
+  }
+  if (nameOfTheGame === 'brain-calc') {
+    const pairOfNumbers = cons(getRandomNum(1, 20), getRandomNum(1, 10));
+    const getFirstArg = car(pairOfNumbers);
+    const getSecondArg = cdr(pairOfNumbers);
+    const strOfOperations = '+-*';
+    const lengthOfStr = strOfOperations.length;
+    const randomIndex = getRandomNum(0, lengthOfStr);
+    const operation = strOfOperations[randomIndex];
+    switch (operation) {
+      case '+': {
+        const question = `${getFirstArg} + ${getSecondArg}`;
+        const correctAnswer = getFirstArg + getSecondArg;
+        return cons(question, correctAnswer);
+      }
+      case '-': {
+        const question = `${getFirstArg} - ${getSecondArg}`;
+        const correctAnswer = getFirstArg - getSecondArg;
+        return cons(question, correctAnswer);
+      }
+      case '*': {
+        const question = `${getFirstArg} * ${getSecondArg}`;
+        const correctAnswer = getFirstArg * getSecondArg;
+        return cons(question, correctAnswer);
+      }
+      // no default
+    }
+  }
+};
+
+const game = (nameOfTheGame) => {
+  start(nameOfTheGame);
+  const name = readlineSync.question('May I have your name? ');
+  console.log(greeting(name));
+  for (let round = 1; round <= maxNumberOfRound; round += 1) {
+    const pair = makeQuestionAndCorrectAnswer(nameOfTheGame);
+    const question = car(pair);
+    console.log(`Question: ${question}`);
+    const correctAnswer = cdr(pair);
     const answer = readlineSync.question('Your answer: ');
-    if (answer === correctAnswer) {
+    const correctAnswerToString = toString(correctAnswer);
+    if (answer === correctAnswerToString) {
       console.log('Correct!');
     } else {
-      console.log(`'${answer}' was wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+      console.log(`'${answer}' was wrong answer ;(. Correct answer was '${correctAnswerToString}'`);
+      console.log(`Let's try again, ${name}!`);
       return;
     }
   }
   console.log(`Congratulations, ${name}!`);
 };
 
-export { evenGame };
+const evenGame = () => game('brain-even');
+
+const calc = () => game('brain-calc');
+
+export { evenGame, calc };
 export default hello;
